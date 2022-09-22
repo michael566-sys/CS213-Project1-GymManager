a2e576fae6328c4ee17a959776a7608c7da3e94e
Songyuan Zhang <songyuanzhang@nbp-196-69.nbp.ruw.rutgers.edu>
2022-09-22 18:09:05 -0400

    Second commit -Songyuan

---
+++ src/project1/Member.java
@@ -0,0 +1,46 @@
+package project1;
+
+public class Member {
+    private String fname;
+    private String lname;
+    private Date dob;
+    private Date expire;
+    private Location location;
+
+    public Member() {
+        this.dob = new Date();
+    }
+    @Override
+    public String toString() {
+        //April March, DOB: 3/31/1990, Membership expires 6/30/2023, Location: PISCATAWAY, 08854, MIDDLESEX
+        return this.fname + ", " + this.lname + ": " + this.dob + ", " + "Membership expires " + expire + ", " + "Location: " + this.location;
+    };
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Member) {
+            Member member = (Member) obj; //casting
+            if (member.fname.toLowerCase().equals(this.fname.toLowerCase()) &
+                    member.lname.toLowerCase().equals(this.lname.toLowerCase()) &
+                    member.dob.compareTo(this.dob) == 0
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public int compareTo(Member member) {
+        if (member.fname.equals(this.fname)) {
+
+    }
+
+
+    //testbed main
+    public static void main(String[] args) {
+        //test case for equals
+        //
+        //test case for compareTo
+
+        System.out.println();
+    }
+}
